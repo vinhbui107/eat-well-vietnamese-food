2629aacc5f64052df32be6e35c503c286eee950d
vinhbui107 <vinhbui.tg@gmail.com>
2020-05-19 23:17:02 +0700

    Add Swagger

--- Eat-Well.Web/Startup.cs
+++ Eat-Well.Web/Startup.cs
@@ -5,6 +5,8 @@ using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using System;
 
 namespace Eat_Well.Web
 {
@@ -26,6 +28,50 @@ namespace Eat_Well.Web
             {
                 configuration.RootPath = "ClientApp/dist";
             });
+
+            #region -- Swagger --            
+            var inf1 = new OpenApiInfo
+            {
+                Title = "API v1.0",
+                Version = "v1",
+                Description = "Swashbuckle",
+                TermsOfService = new Uri("http://appointvn.com"),
+                Contact = new OpenApiContact
+                {
+                    Name = "Vinh Bui",
+                    Email = "vinhbui.tg@gmail.com"
+                },
+                License = new OpenApiLicense
+                {
+                    Name = "Apache 2.0",
+                    Url = new Uri("http://www.apache.org/licenses/LICENSE-2.0.html")
+                }
+            };
+
+            var inf2 = new OpenApiInfo
+            {
+                Title = "API v2.0",
+                Version = "v2",
+                Description = "Swashbuckle",
+                TermsOfService = new Uri("http://appointvn.com"),
+                Contact = new OpenApiContact
+                {
+                    Name = "Vinh Bui",
+                    Email = "vinhbui.tg@gmail.com"
+                },
+                License = new OpenApiLicense
+                {
+                    Name = "Apache 2.0",
+                    Url = new Uri("http://www.apache.org/licenses/LICENSE-2.0.html")
+                }
+            };
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", inf1);
+                c.SwaggerDoc("v2", inf2);
+            });
+            #endregion
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,6 +87,17 @@ namespace Eat_Well.Web
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            #region -- Swagger --
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1.0");
+                c.SwaggerEndpoint("/swagger/v2/swagger.json", "API v2.0");
+            });
+            #endregion
+
+            app.UseCors(p => p.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
