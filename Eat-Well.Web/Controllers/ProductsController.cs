98c8b678947b93e08b6f6f91ebaf762b47f70597
lethienbao99 <1754050012bao@ou.edu.vn>
2020-05-23 23:00:18 +0700

    ADD Products Controller

---
+++ Eat-Well.Web/Controllers/ProductsController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Eat_Well.Web.Controllers
+{
+    using BLL;
+    using DAL.Models;
+    using Common.Req;
+    using Common.Rsp;
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        public ProductsController()
+        {
+            _svc = new ProductsSvc();
+        }
+
+        [HttpGet("{Id}")]
+        public IActionResult getProductById(int Id)
+        {
+            var res = new SingleRsp();
+            res = _svc.Read(Id);
+            return Ok(res);
+        }
+
+        [HttpGet]
+        public IActionResult getAllProducts()
+        {
+            var res = new SingleRsp();
+            res.Data = _svc.All;
+            return Ok(res);
+        }
+
+        [HttpPost]
+        public IActionResult CreateProduct([FromBody]ProductsReq req)
+        {
+            var res = _svc.CreateProduct(req);
+            return Ok(res);
+        }
+
+        [HttpPut]
+        public IActionResult UpdateProduct([FromBody]ProductsReq req)
+        {
+            var res = _svc.UpdateProduct(req);
+            return Ok(res);
+        }
+
+        [HttpDelete]
+        public IActionResult RemoveProduct([FromBody]ProductsReq req)
+        {
+            var res = _svc.RemoveProduct(req);
+            return Ok(res);
+        }
+
+        private readonly ProductsSvc _svc;
+    }
+}
