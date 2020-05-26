f8664dd907c01ddf4a3bd7fe309d06736abae9f4
vinhbui107 <vinhbui.tg@gmail.com>
2020-05-27 00:58:03 +0700

    Create API for categories

---
+++ Eat-Well.BLL/CategoriesSvc.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+using Eat_Well.BLL;
+using Eat_Well.Common.Rsp;
+using Eat_Well.Common.BLL;
+using Eat_Well.Common.Req;
+
+namespace Eat_Well.BLL
+{
+    using DAL;
+    using DAL.Models;
+
+    public class CategoriesSvc: GenericSvc<CategoriesRep, Categories>
+    {
+        #region -- Overrides --
+
+        /// <summary>
+        /// Read single object
+        /// </summary>
+        /// <param name="id">Primary key</param>
+        /// <returns>Return the object</returns>
+        public override SingleRsp Read(int id)
+        {
+            var res = new SingleRsp();
+
+            var m = _rep.Read(id);
+
+            res.Data = m;
+
+            return res;
+        }
+
+        /// <summary>
+        /// Update
+        /// </summary>
+        /// <param name="m">The model</param>
+        /// <returns>Return the result</returns>
+        public override SingleRsp Update(Categories m)
+        {
+            var res = new SingleRsp();
+
+            var m1 = m.CategoryId > 0 ? _rep.Read(m.CategoryId) : _rep.Read(m.CategoryName);
+            if (m1 == null)
+            {
+                res.SetError("EZ103", "No data.");
+            }
+            else
+            {
+                res = base.Update(m);
+                res.Data = m;
+            }
+
+            return res;
+        }
+        #endregion
+
+        #region -- Methods --
+
+        /// <summary>
+        /// Initialize
+        /// </summary>
+        public CategoriesSvc() { }
+
+
+        #endregion
+
+        #region -- Create Product --
+
+        public SingleRsp CreateCategory(CategoriesReq cate)
+        {
+            var res = new SingleRsp();
+            Categories categories = new Categories();
+            categories.CategoryId = cate.CategoryId;
+            categories.CategoryName = cate.CategoryName;
+            categories.CategorySlug = cate.CategorySlug;
+            res = _rep.CreateCategory(categories);
+            return res;
+        }
+        #endregion
+        //===========================================================
+        //===========================================================
+        #region -- Update Product --
+
+        public SingleRsp UpdateCategory(CategoriesReq cate)
+        {
+            var res = new SingleRsp();
+            Categories categories = new Categories();
+            categories.CategoryId = cate.CategoryId;
+            categories.CategoryName = cate.CategoryName;
+            categories.CategorySlug = cate.CategorySlug;
+            res = _rep.UpdateCategory(categories);
+            return res;
+        }
+        #endregion
+        //===========================================================
+        //===========================================================
+        #region -- Delete Product --
+
+        public SingleRsp RemoveCategory(CategoriesReq cate)
+        {
+            var res = new SingleRsp();
+            Categories categories = new Categories();
+            categories.CategoryId = cate.CategoryId;
+            categories.CategoryName = cate.CategoryName;
+            categories.CategorySlug = cate.CategorySlug;
+            res = _rep.RemoveCategory(categories);
+               return res;
+        }
+        #endregion
+
+
+    }
+}
---
+++ Eat-Well.DAL/CategoriesRep.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+using Eat_Well.Common.DAL;
+using Eat_Well.Common.Rsp;
+using Eat_Well.DAL.Models;
+
+
+namespace Eat_Well.DAL
+{
+    public class CategoriesRep: GenericRep<EatWellDBContext, Categories>
+    {
+        #region -- Overrides --
+
+        /// <summary>
+        /// Read single object
+        /// </summary>
+        /// <param name="id">Primary key</param>
+        /// <returns>Return the object</returns>
+        public override Categories Read(int id)
+        {
+            var res = All.FirstOrDefault(p => p.CategoryId == id);
+            return res;
+        }
+
+
+        /// <summary>
+        /// Remove and not restore
+        /// </summary>
+        /// <param name="id">Primary key</param>
+        /// <returns>Number of affect</returns>
+        public int Remove(int id)
+        {
+            var m = base.All.First(i => i.CategoryId == id);
+            m = base.Delete(m); //TODO
+            return m.CategoryId;
+        }
+
+        #endregion
+
+        #region -- Methods --
+
+        /// <summary>
+        /// Initialize
+        /// </summary>
+
+        #region MyRegion
+        public SingleRsp CreateCategory(Categories cate)
+        {
+            var res = new SingleRsp();
+            using (var context = new EatWellDBContext())
+            {
+                using (var tran = context.Database.BeginTransaction())
+                {
+                    try
+                    {
+                        var t = context.Categories.Add(cate);
+                        context.SaveChanges();
+                        tran.Commit();
+                    }
+                    catch (Exception e)
+                    {
+                        tran.Rollback();
+                        res.SetError(e.StackTrace);
+                    }
+                }
+            }
+
+            return res;
+        }
+
+        #endregion
+        //=================================================================
+        //=================================================================
+        //=================================================================
+
+        #region MyRegion
+
+        public SingleRsp UpdateCategory(Categories cate)
+        {
+            var res = new SingleRsp();
+            using (var context = new EatWellDBContext())
+            {
+                using (var tran = context.Database.BeginTransaction())
+                {
+                    try
+                    {
+                        var t = context.Categories.Update(cate);
+                        context.SaveChanges();
+                        tran.Commit();
+                    }
+                    catch (Exception e)
+                    {
+                        tran.Rollback();
+                        res.SetError(e.StackTrace);
+                    }
+                }
+            }
+
+            return res;
+        }
+        #endregion
+        //=================================================================
+        //=================================================================
+        //=================================================================
+
+        #region -- Delete Product --
+        public SingleRsp RemoveCategory(Categories cate)
+        {
+            var res = new SingleRsp();
+            using (var context = new EatWellDBContext())
+            {
+                using (var tran = context.Database.BeginTransaction())
+                {
+                    try
+                    {
+                        var t = context.Categories.Remove(cate);
+                        context.SaveChanges();
+                        tran.Commit();
+                    }
+                    catch (Exception ex)
+                    {
+                        tran.Rollback();
+                        res.SetError(ex.StackTrace);
+                    }
+                }
+            }
+            return res;
+        }
+        #endregion
+
+        #endregion
+    }
+}
