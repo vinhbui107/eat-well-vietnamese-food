509a391e47fb35ef5fc8e206dffcf30ce7d44ba6
vinhbui107 <vinhbui.tg@gmail.com>
2020-06-11 14:05:44 +0700

    Add API for Users

---
+++ Eat-Well.Common/Req/UsersReq.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Eat_Well.Common.Req
+{
+    public class UsersReq
+    {
+        public int UserId { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string FullName { get; set; }
+        public string Password { get; set; }
+        public string Address { get; set; }
+        public int? Phone { get; set; }
+        public bool IsAdmin { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
