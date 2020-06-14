c46cc08fc1d2fe93fbacf990cb7a5426f0fad957
dthanhbinh0706 <dthanhbinh0706@gmail.com>
2020-06-14 21:29:46 +0700

    About us

---
+++ Eat-Well.Web/ClientApp/src/app/about/assets/js/scripts.js
@@ -0,0 +1,39 @@
+
+function scroll_to(clicked_link, nav_height) {
+	var element_class = clicked_link.attr('href').replace('#', '.');
+	var scroll_to = 0;
+	if(element_class != '.top-content') {
+		element_class += '-container';
+		scroll_to = $(element_class).offset().top - nav_height;
+	}
+	if($(window).scrollTop() != scroll_to) {
+		$('html, body').stop().animate({scrollTop: scroll_to}, 1000);
+	}
+}
+
+
+jQuery(document).ready(function() {
+	
+	/*
+	    Navigation
+	*/
+	$('a.scroll-link').on('click', function(e) {
+		e.preventDefault();
+		scroll_to($(this), $('nav').outerHeight());
+	});
+	// toggle "navbar-no-bg" class
+	$('.top-content .carousel-caption').waypoint(function() {
+		$('nav').toggleClass('navbar-no-bg');
+	});
+	
+    /*
+        Background slideshow
+    */
+    $('.section-4-container').backstretch("assets/img/backgrounds/3.jpg");
+    
+    /*
+	    Wow
+	*/
+	new WOW().init();
+	
+});
