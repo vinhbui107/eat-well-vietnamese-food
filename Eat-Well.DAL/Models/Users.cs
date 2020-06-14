d502600c26154a353db49a6dd26d47213401a230
vinhbui107 <vinhbui.tg@gmail.com>
2020-06-15 00:03:16 +0700

    Update phone field in DB

--- Eat-Well.DAL/Models/Users.cs
+++ Eat-Well.DAL/Models/Users.cs
@@ -16,7 +16,7 @@ namespace Eat_Well.DAL.Models
         public string FullName { get; set; }
         public string Password { get; set; }
         public string Address { get; set; }
-        public int? Phone { get; set; }
+        public string Phone { get; set; }
         public bool IsAdmin { get; set; }
         public bool IsActive { get; set; }
 
