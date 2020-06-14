5a30992b24f49482e47a1a69f15557930bd03551
vinhbui107 <vinhbui.tg@gmail.com>
2020-06-14 23:02:39 +0700

    Add methods for ProductOptions

---
+++ Eat-Well.Common/Req/ProductOptionsReq.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Eat_Well.Common.Req
+{
+    public class ProductOptionsReq
+    {
+        public int OptionId { get; set; }
+        public int ProductId { get; set; }
+        public int Price { get; set; }
+    }
+}
