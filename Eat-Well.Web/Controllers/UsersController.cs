83dec525d5668c62edaeacaadd4cfb7145c8e15e
vinhbui107 <vinhbui.tg@gmail.com>
2020-06-19 23:58:31 +0700

    Delete import unuse

--- Eat-Well.Web/Controllers/UsersController.cs
+++ Eat-Well.Web/Controllers/UsersController.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using System.IdentityModel.Tokens.Jwt;
 
 namespace Eat_Well.Web.Controllers
 {
